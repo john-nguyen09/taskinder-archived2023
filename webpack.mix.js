0f59ea313087ff8f6e7627bf190918414ee2d184
John Nguyen <john@mindatlas.com>
2020-11-26 13:20:44 +1100

    Move chunks file into chunks folder,
    - include chunkhash to automatically clear cache
    - use devtool for production

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,8 @@ const mix = require('laravel-mix');
 
 require('laravel-mix-polyfill');
 
+require('laravel-mix-clean');
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -18,13 +20,29 @@ mix
     .sourceMaps()
     .sass('resources/sass/app.scss', 'public/css')
     .copyDirectory('resources/img', 'public/img')
+    .clean({
+        cleanOnceBeforeBuildPatterns: [
+            '**/*',
+            '!.htaccess',
+            '!favicon.ico',
+            '!index.php',
+            '!robots.txt',
+            '!web.config',
+        ],
+    })
     .polyfill({
         enabled: true,
         useBuiltIns: "usage",
         targets: { "firefox": "50", "ie": 11 }
     });
 
-mix.sourceMaps(false, 'source-map');
+mix.webpackConfig({
+    output: {
+        chunkFilename: 'js/chunks/[name].js?id=[chunkhash]',
+    },
+});
+
+mix.sourceMaps(true, 'inline-cheap-source-map', 'source-map');
 
 if (mix.inProduction()) {
     mix.version();
